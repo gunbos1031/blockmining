292f2125e2c99519c48c561f7ea1dcf33c4873e3
gunbos1031 <gunbos1031@gmail.com>
2021-07-28 14:11:12 +0000

    react; add Header and Payload box

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './app.css';
+import React from 'react';
+import styles from './app.module.css';
+import Header from './header/header'
+import Payload from './payload/payload'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className={styles.container}>
+			<Header />
+			<Payload />
+		</div>
+	)
 }
 
 export default App;
---
+++ client/src/payload/payload.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './payload.module.css';
+
+const Payload = () => {
+	return (
+		<section className={styles.section}>
+			<form method="POST" className={styles.form}>
+				<input 
+					className={styles.data}
+					type="text" 
+					placeholder="Data for you Block"
+					name="blockData"
+					required
+				/>
+				<input 
+					className={styles.diff}
+					type="text" 
+					placeholder="Difficulty for your Block" 
+					name="blockDifficulty"
+					required
+				/>
+				<button className={styles.submit}><i className="fas fa-fire"></i></button>
+			</form>
+		</section>
+	)
+}
+
+export default Payload
